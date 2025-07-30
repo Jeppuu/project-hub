665fea5a7c453daff0e76ca6556f6971cad9fde6
Jeppu <blomqvist.jennam@gmail.com>
2025-07-30 15:58:02 +0300

    give progressLoader a border

--- src/components/rgb-guess/ProgressLoader.tsx
+++ src/components/rgb-guess/ProgressLoader.tsx
@@ -12,9 +12,9 @@ const ProgressLoader = React.memo(
     max,
     colorClass = "gradient-to-r-custom",
   }: ProgressLoaderProps) => (
-    <div className="w-full h-2 bg-overlay-1 rounded mb-2 overflow-hidden">
+    <div className="w-full h-4 bg-overlay-1 rounded mb-2 overflow-hidden border-2 border-crust">
       <div
-        className={`h-full transition-all duration-200 ${colorClass}`}
+        className={`h-full transition-all duration-200 ${colorClass} border-r-2 border-crust`}
         style={{
           width: `${Math.min((value / max) * 100, 100)}%`,
         }}
