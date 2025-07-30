98610bbd559da8e914b5f4fbd51c203928c4670f
Jeppu <blomqvist.jennam@gmail.com>
2025-07-30 15:58:50 +0300

    adjust header color based on bg color for better readability

--- src/components/rgb-guess/rgbHeader.tsx
+++ src/components/rgb-guess/rgbHeader.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 interface RgbHeaderProps {
   rgb: string;
@@ -6,21 +6,45 @@ interface RgbHeaderProps {
   headerColor: string;
 }
 
+const textForDark = "text-text";
+const textForLight = "text-base";
+
 const RgbHeader = React.memo(
   ({ rgb, message, headerColor }: RgbHeaderProps) => {
     const isGradient = headerColor === "gradient";
+
+    // Compare the headerColor string to see if it's brightness is greater than 128 using the formula:
+    // brightness = 0.2126R+0.7152G+0.0722B
+    // to determine if the text should be light or dark
+    const calculateBrightness = useCallback((color: string) => {
+      const [r, g, b] = color
+        .slice(4, -1)
+        .split(",")
+        .map((c) => parseInt(c.trim(), 10));
+      const brightness = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+      const textColorClass = brightness > 128 ? textForLight : textForDark;
+      return textColorClass;
+    }, []);
+
+    const textColorClass = React.useMemo(
+      () => (isGradient ? textForLight : calculateBrightness(headerColor)),
+      [isGradient, headerColor, calculateBrightness]
+    );
+
     return (
       <div
         className={`${
           isGradient ? "gradient-to-r-custom" : ""
-        } w-full rounded-xl p-4 text-base text-center mb-2 border-2 border-crust drop-shadow-solid-crust transition-colors duration-300`}
+        } w-full rounded-xl p-4 ${textColorClass} text-center mb-2 border-2 border-crust drop-shadow-solid-crust transition-colors duration-300`}
         style={isGradient ? {} : { backgroundColor: headerColor }}
       >
-        <h2 className="text-xl font-bold mb-1">Guess the Color!</h2>
-        <div className="text-2xl font-mono font-bold tracking-wider mb-2">
+        <h2 className="text-xl font-bold mb-1 text-shadow-sm">
+          Guess the Color!
+        </h2>
+        <div className="text-3xl text-shadow-md font-mono font-bold tracking-wider p-2 mb-2">
           {rgb.toUpperCase()}
         </div>
-        <div className="text-base font-pixel text-crust">{message}</div>
+        <div className="text-shadow-sm font-pixel ">{message}</div>
       </div>
     );
   }
